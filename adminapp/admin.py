7bfe584f3a4989630940c069b7034c6ab92e66b0
kevin <kevinjoseminj98@gmail.com>
2022-10-14 15:20:40 +0530

    changed database

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Administrator,Library,Reviews
+
+admin.site.register(Reviews)
+admin.site.register(Administrator)
+admin.site.register(Library)
+
--- guest_library/settings.py
+++ guest_library/settings.py
@@ -80,13 +80,8 @@ WSGI_APPLICATION = 'guest_library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'guest',
-        'USER': 'root',
-        'PASSWORD': 'root@123',
-        'HOST': 'localhost',
-        'PORT': 3306
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
